ee5609db335cb13cb5509f0383a5d439f323c972
Michał Woźniczak <michal.wozniczak@gmail.com>
2017-02-27 21:29:56 +0100

    Added rudimentary content edition to SceneNodeWidget

--- build/widgets.js
+++ build/widgets.js
@@ -7,18 +7,33 @@ var __extends = (this && this.__extends) || function (d, b) {
 var storm_react_diagrams_1 = require("storm-react-diagrams");
 var _ = require("lodash");
 var React = require("react");
-var div = React.DOM.div;
+var content_previewers_1 = require("./content_previewers");
+var div = React.DOM.div, span = React.DOM.span, textarea = React.DOM.textarea, b = React.DOM.b;
 var SceneNodeWidget = (function (_super) {
     __extends(SceneNodeWidget, _super);
     function SceneNodeWidget(props) {
         var _this = _super.call(this, props) || this;
-        console.log(props.node);
-        console.log(props.node.content);
+        _this.state = SceneNodeWidget.defaultState;
+        _this.state.content = props.node.content;
+        _this.previewer = content_previewers_1.markdown_previewer;
+        _this.toggleEdit = _this.toggleEdit.bind(_this);
+        _this.editContent = _this.editContent.bind(_this);
         return _this;
     }
+    SceneNodeWidget.prototype.toggleEdit = function () {
+        this.setState({ beingEdited: !this.state.beingEdited });
+    };
+    SceneNodeWidget.prototype.editContent = function (evt) {
+        this.props.node.content = evt.target.value;
+        this.setState({ content: evt.target.value });
+    };
     SceneNodeWidget.prototype.render = function () {
         var node = this.props.node;
-        return (div({ className: 'basic-node story-node', style: { background: node.color } }, div({ className: 'title' }, div({ className: 'name' }, node.name), div({ className: 'fa fa-close', onClick: node.remove })), div({ className: 'content' }, node.content), div({ className: 'ports' }, div({ className: 'in' }, _.map(node.getInPorts(), function (port) {
+        console.log('wololo');
+        return (div({ className: 'basic-node story-node', style: { background: node.color } }, div({ className: 'title' }, span({ className: 'name' }, b({}, 'Id: '), node.name), span({
+            className: 'btn btn-default btn-sm glyphicon glyphicon-pencil' + (this.state.beingEdited ? ' active' : ''),
+            onClick: this.toggleEdit
+        }), span({ className: 'btn btn-default btn-sm glyphicon glyphicon-remove-sign', onClick: node.remove })), div({ className: 'content-bar' }), this.state.beingEdited || div({ className: 'content', dangerouslySetInnerHTML: { __html: this.previewer(node.content) } }), !this.state.beingEdited || textarea({ value: this.state.content, onChange: this.editContent }), div({ className: 'title' }, span({ className: 'name' }, 'Choices:'), span({ className: 'btn btn-default btn-sm glyphicon glyphicon-plus' })), div({ className: 'ports' }, div({ className: 'in' }, _.map(node.getInPorts(), function (port) {
             return React.createElement(storm_react_diagrams_1.DefaultPortLabel, { model: port });
         })), div({ className: 'out' }, _.map(node.getOutPorts(), function (port) {
             return React.createElement(storm_react_diagrams_1.DefaultPortLabel, { model: port });
@@ -29,6 +44,10 @@ var SceneNodeWidget = (function (_super) {
 SceneNodeWidget.defaultProps = {
     node: null,
     diagramEngine: null
+};
+SceneNodeWidget.defaultState = {
+    beingEdited: false,
+    content: ''
 };
 exports.SceneNodeWidget = SceneNodeWidget;
 //# sourceMappingURL=widgets.js.map
--- src/widgets.ts
+++ src/widgets.ts
@@ -1,52 +1,93 @@
 import {
     DefaultNodeWidget,
-	DefaultNodeState,
+    DefaultNodeState,
     DefaultPortLabel,
-	DiagramEngine
+    DiagramEngine
 } from 'storm-react-diagrams'
 import * as _ from 'lodash'
 import * as React from 'react'
 
 import {SceneNodeModel} from './models'
+import {markdown_previewer} from './content_previewers'
 
-var div = React.DOM.div
+var div = React.DOM.div,
+    span = React.DOM.span,
+    textarea = React.DOM.textarea,
+    b = React.DOM.b
 
 export interface SceneNodeProps {
-	node: SceneNodeModel,
-	diagramEngine: DiagramEngine
+    node: SceneNodeModel,
+    diagramEngine: DiagramEngine
 }
 
-export class SceneNodeWidget extends React.Component<SceneNodeProps, DefaultNodeState> {
+export interface SceneNodeState {
+    beingEdited: boolean,
+    content: string
+}
 
-	public static defaultProps: SceneNodeProps = {
-		node: null,
-		diagramEngine: null
-	};
+export class SceneNodeWidget extends React.Component<SceneNodeProps, SceneNodeState> {
 
-	constructor(props: SceneNodeProps) {
-		super(props)
-		console.log(props.node)
-		console.log(props.node.content)
-	}
+    public static defaultProps: SceneNodeProps = {
+        node: null,
+        diagramEngine: null
+    };
+
+    public static defaultState: SceneNodeState = {
+        beingEdited: false,
+        content: ''
+    }
+
+    previewer: any
+
+    constructor(props: SceneNodeProps) {
+        super(props)
+        this.state = SceneNodeWidget.defaultState
+        this.state.content = props.node.content
+        this.previewer = markdown_previewer
+        this.toggleEdit = this.toggleEdit.bind(this)
+        this.editContent = this.editContent.bind(this)
+    }
+
+    toggleEdit() {
+        this.setState({beingEdited: !this.state.beingEdited})
+    }
+
+    editContent(evt) {
+        this.props.node.content = evt.target.value
+        this.setState({content: evt.target.value})
+    }
+
     render() {
-		let node = this.props.node
-		
-		return (
-			div({className: 'basic-node story-node', style: {background: node.color }},
-				div({className:'title'},
-					div({className:'name'}, node.name),
-					div({className: 'fa fa-close', onClick: node.remove})
-				),
-				div({className: 'content'}, node.content),
-				div({className:'ports'},
-					div({className: 'in'}, _.map(node.getInPorts(),(port) => {
-						return React.createElement(DefaultPortLabel,{model: port});
-					})),
-					div({className: 'out'}, _.map(node.getOutPorts(),(port) => {
-						return React.createElement(DefaultPortLabel,{model: port});
-					})),
-				)
-			)
-		);
-	}
+        let node = this.props.node
+        console.log('wololo')
+        
+        return (
+            div({className: 'basic-node story-node', style: {background: node.color }},
+                div({className:'title'},
+                    span({className:'name'}, b({}, 'Id: '), node.name),
+                    span({
+                        className: 'btn btn-default btn-sm glyphicon glyphicon-pencil' + (this.state.beingEdited ? ' active' : ''),
+                        onClick: this.toggleEdit
+                    }),
+                    span({className: 'btn btn-default btn-sm glyphicon glyphicon-remove-sign', onClick: node.remove}),
+                ),
+                div({className: 'content-bar'},
+                ),
+                this.state.beingEdited || div({className: 'content', dangerouslySetInnerHTML: {__html: this.previewer(node.content)}}),
+                !this.state.beingEdited || textarea({value: this.state.content, onChange: this.editContent}),
+                div({className:'title'},
+                    span({className: 'name'}, 'Choices:'),
+                    span({className: 'btn btn-default btn-sm glyphicon glyphicon-plus'})
+                ),
+                div({className:'ports'},
+                    div({className: 'in'}, _.map(node.getInPorts(),(port) => {
+                        return React.createElement(DefaultPortLabel,{model: port});
+                    })),
+                    div({className: 'out'}, _.map(node.getOutPorts(),(port) => {
+                        return React.createElement(DefaultPortLabel,{model: port});
+                    })),
+                )
+            )
+        );
+    }
 }
