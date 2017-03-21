d24245e9d7fae10a8544ebd4657132e0bf0afd54
Michał Woźniczak <michal.wozniczak@gmail.com>
2017-03-21 23:06:21 +0100

    Added an "in" port for scene models (doesn't work yet!)

--- src/models.ts
+++ src/models.ts
@@ -6,14 +6,17 @@ import * as _ from "lodash";
 
 export class SceneNodeModel extends NodeModel {
     
-    content: string
+    content: any
+    inPort: PortModel
     ports: {[s: string]: ChoicePortModel}
     name: string
 
     
-    constructor(name: string = 'Scene title', content?: string) {
+    constructor(name: string = 'Scene title', content?: any) {
         super('story')
         this.name = name
+        this.inPort = new PortModel('In')
+        this.inPort.setParentNode(this)
         if (content === undefined) {
             this.content = `Placeholder content for **${this.name}**`
         }
@@ -23,12 +26,14 @@ export class SceneNodeModel extends NodeModel {
 		super.deSerialize(object)
 		this.content = object.content
         this.name = object.name
+        this.inPort = this.inPort.deSerialize()
 	}
 	
 	serialize(){
 		return _.merge(super.serialize(), {
 			content: this.content,
-            name: this.name
+            name: this.name,
+            inPort: this.inPort.serialize()
 		});
 	}
 }
